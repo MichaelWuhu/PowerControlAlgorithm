cba182dd388d3c81c74a09e32a2fb77da1dac8ba
ElijahRosal <148596994+ElijahRosal@users.noreply.github.com>
2023-12-08 18:51:42 -0800

    added the user interactive part

--- PowerTester.java
+++ PowerTester.java
@@ -1,5 +1,40 @@
 public class PowerTester {
     public static void main(String[] args) {
-        System.out.println("WEEEEEEEEEEEEEEEEe");
+		//User interactive part
+		String option1;
+		
+		while(true){// Application menu to be displayed to the user.
+			System.out.println("Select an option:");
+			System.out.println("Type \"A\" Add an appliance");
+			System.out.println("Type \"D\" Delete an appliance");	
+			System.out.println("Type \"L\" List the appliances");
+			System.out.println("Type \"F\" Read Appliances from a file");
+			System.out.println("Type \"S\" To Start the simulation");
+			System.out.println("Type \"Q\" Quit the program");
+			option1=scan.nextLine();
+			
+			if (option1.equals("A")) {
+				
+			}
+			else if (option1.equals("D")) {
+				
+			}
+			else if (option1.equals("L")) {
+				
+			}
+			else if (option1.equals("F")) {
+				
+			}
+			else if (option1.equals("S")) {
+				
+			}
+			else if (option1.equals("Q")) {
+				System.exit(0);
+			}
+			else {
+				System.out.println("Invalid Input");
+			}
+			
+		}
     }
 }
