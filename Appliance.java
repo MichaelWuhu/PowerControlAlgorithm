a066d26499a6a058318f5f7caf9fe1c37e45532e
Michael <mwu250@gmail.com>
2023-12-08 18:32:37 -0800

    finished making appliance-class

--- Appliance.java
+++ Appliance.java
@@ -1,16 +1,64 @@
+import java.util.Random;
+
 public class Appliance {
-    
     private String type;
-    private int onWattageUsed;
-    private double probOn;
-    private int location;
-    private int ID;
-    
-    public Appliance(String type, int onWattageUsed, double probOn, int location, int ID){
+    private int onWattage;
+    private double probability;
+    private long location;
+    private String applianceId;
+    private boolean isOn;
+
+    // Constructor
+    public Appliance(String type, int onWattage, double probability, long location, String applianceId) {
         this.type = type;
-        this.onWattageUsed = onWattageUsed;
-        this.probOn = probOn;
+        this.onWattage = onWattage;
+        this.probability = probability;
         this.location = location;
-        this.ID = ID;
+        this.applianceId = applianceId;
+        this.isOn = false; // default state is off
+    }
+
+    // Getter methods
+    public String getType() {
+        return type;
+    }
+
+    public int getOnWattage() {
+        return onWattage;
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public long getLocation() {
+        return location;
+    }
+
+    public String getApplianceId() {
+        return applianceId;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    // Setter method to turn the appliance on or off based on probability
+    public void updateStatus() {
+        Random random = new Random();
+        double randomValue = random.nextDouble();
+
+        // check probability to turn on/off appliance
+        isOn = randomValue < probability;
+    }
+
+    // Method to get current wattage based on appliance state
+    public int getCurrentWattage() {
+        return isOn ? onWattage : 0;
+    }
+
+    // Method to get current state of appliance (ON/OFF)
+    public String getCurrentState() {
+        return isOn ? "ON" : "OFF";
     }
 }
