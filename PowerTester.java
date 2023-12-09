71b027791f8347030d49aef68d4d1a07636edab8
ElijahRosal <148596994+ElijahRosal@users.noreply.github.com>
2023-12-08 19:25:43 -0800

    imported relevant classes and added static ArrayLists and systemIn scanner for later use

--- PowerTester.java
+++ PowerTester.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
+
 public class PowerTester {
+	static ArrayList<SmartAppliance> smartList = new ArrayList<>();
+	static ArrayList<Appliance> appList = new ArrayList<>();
+	static ArrayList<Appliance> wholeArea = new ArrayList<>();
+	static ArrayList<ArrayList<Appliance>> divArea = new ArrayList<>();
+	static ArrayList<Long> locationList = new ArrayList<>();
+	static Scanner scnr = new Scanner(System.in);
+	
     public static void main(String[] args) {
 		//User interactive part
 		String option1;
@@ -11,7 +22,7 @@ public class PowerTester {
 			System.out.println("Type \"F\" Read Appliances from a file");
 			System.out.println("Type \"S\" To Start the simulation");
 			System.out.println("Type \"Q\" Quit the program");
-			option1=scan.nextLine();
+			option1 = scnr.nextLine();
 			
 			if (option1.equals("A")) {
 				
