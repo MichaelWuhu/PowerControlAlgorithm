ea7e14ddd10d0015877e07d5fb7f5d2a8e858dda
ElijahRosal <148596994+ElijahRosal@users.noreply.github.com>
2023-12-09 23:24:12 -0800

    updated the simulation method and added some outputs

--- PowerTester.java
+++ PowerTester.java
@@ -11,6 +11,38 @@ public class PowerTester {
 	static ArrayList<Appliance> onApps = new ArrayList<>();
 	static ArrayList<SmartAppliance> smartOnApps = new ArrayList<>();
 	static Scanner scnr = new Scanner(System.in);
+	static String folderName = "PowerGrid";
+	static String directoryPath = "C:/";
+	
+	public static void fileWrite(int time) {
+		// Names for the file path
+        String fileName = "TimeStep " + (time+1) + ".txt";
+
+        // Create the folder
+        File directory = new File(directoryPath, folderName);
+        directory.mkdirs();
+
+        // Combine the directory path and file name to create the complete file path
+        String filePath = directoryPath + folderName + "/" + fileName;
+        try {
+            // Create a FileWriter object to write to the file
+            FileWriter writer = new FileWriter(filePath);
+
+            // Write and print the current time step
+            for (int i = 0; i < divArea.size(); ++i) {
+            	ArrayList<Appliance> locale = divArea.get(i);
+            	for (int j = 0; j < locale.size(); ++j) {
+            		writer.write(locale.get(j).toString());
+                    writer.write(System.lineSeparator());
+            	}
+            	
+            }
+            writer.close();
+        } catch (IOException e) {
+            // Handle IO exception
+            e.printStackTrace();
+        }
+	}
 	
 	public static void readAppFile(String file) {
 		try {
@@ -22,29 +54,39 @@ public class PowerTester {
 					long location = parse.nextLong();
 					String name = parse.next();
 					int powerUse = parse.nextInt();
-					double state = parse.nextDouble();
+					double prob = parse.nextDouble();
 					boolean type = parse.nextBoolean();
 					double percent = parse.nextDouble();
 					locationList.add(location);
 					if (type) {
-						SmartAppliance smartApp = new SmartAppliance(type, powerUse, state, location, name, percent);
+						SmartAppliance smartApp = new SmartAppliance(type, powerUse, prob, location, name, percent);
 						wholeArea.add(smartApp);
 					} else {
-						Appliance App = new Appliance(type, powerUse, state, location, name);
+						Appliance App = new Appliance(type, powerUse, prob, location, name);
 						wholeArea.add(App);
 					}
 				}
 			}
+			
+			for (int i = 0; i < locationList.size(); ++i) {
+				for (int j = i+1; j < locationList.size(); ++j) {
+					if (locationList.get(i).equals(locationList.get(j))) {
+						locationList.remove(j);
+						--j;
+					}
+				}
+			}
+
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("File not found. Try again.");
 		}
 	}
 	public static void simulation() {
-		int totalWattage = 0;
+		
 		int maxWattage = 0;
 		int timeSteps = 0;
-		int T = 0;
+		//ArrayList<Long> totalLocations = (ArrayList<Long>) locationList.clone();
+		
 		System.out.println("Enter total allowed wattage:");
 		maxWattage = scnr.nextInt();
 		while (maxWattage < 0) {
@@ -59,79 +101,47 @@ public class PowerTester {
 			timeSteps = scnr.nextInt();
 		}
 		
-		for (int i = 0; i < wholeArea.size(); ++i) {
-			wholeArea.get(i).updateStatus();
-			if (wholeArea.get(i).isOn() && wholeArea.get(i).getType()) {
-				totalWattage += wholeArea.get(i).getOnWattage();
-				//smartOnApps.add((SmartAppliance) wholeArea.get(i));
-				
-			} else if (wholeArea.get(i).isOn()) {
-				totalWattage += wholeArea.get(i).getOnWattage();
-				//onApps.add(wholeArea.get(i));
-			}
-		}
+		/////////////////////////////////////////
+		//////// Going through each time step
+		///////////////////////////////////////////////////////////
 		
-		for (int i = 0; i < locationList.size(); ++i) {
-			for (int j = i+1; j < locationList.size(); ++j) {
-				if (locationList.get(i).equals(locationList.get(j))) {
-					locationList.remove(j);
-					--j;
+		for ( int k = 0; k < timeSteps; ++k ) {
+			divArea.clear();
+			ArrayList<Long> totalLocations = (ArrayList<Long>) locationList.clone();
+			int totalWattage = 0;
+			int locationCounter = 0;
+			int T = 0;
+			// Loop to initialise whether an appliance is on or off
+			for (int i = 0; i < wholeArea.size(); ++i) {
+				wholeArea.get(i).setIsOn(false);
+				wholeArea.get(i).updateStatus();
+				if (wholeArea.get(i).isOn() && wholeArea.get(i).getType()) {
+					totalWattage += wholeArea.get(i).getOnWattage();
+					//smartOnApps.add((SmartAppliance) wholeArea.get(i));
+					
+				} else if (wholeArea.get(i).isOn()) {
+					totalWattage += wholeArea.get(i).getOnWattage();
+					//onApps.add(wholeArea.get(i));
 				}
 			}
-		}
-		// intialize the divArea arrayList with the locations and appliances in each location
-		for (int i = 0; i < locationList.size(); ++i) {
-			ArrayList<Appliance> locale = new ArrayList<>();
-			for (int j = 0; j < wholeArea.size(); ++j) {
-				if (locationList.get(i).equals(wholeArea.get(j).getLocation()) && wholeArea.get(j).isOn()) {
-					locale.add(wholeArea.get(j));
-				}
-			}
-			divArea.add(locale);
-		}
-		
-		
-		System.out.println("total Watt: " + totalWattage);
-		while (totalWattage > maxWattage) {
-			totalWattage = 0;
-			smartOnApps.clear();
-			onApps.clear();
 			
-			boolean count = false;
-			
-			for (int i = 0; i < divArea.size(); ++i) {
-				ArrayList<Appliance> currentLocation = divArea.get(i);
-				for (int j = 0; j < currentLocation.size(); ++j) {
-					if (currentLocation.get(j).getType()) {
-						smartOnApps.add((SmartAppliance) currentLocation.get(j));
-					} else {
-						onApps.add(currentLocation.get(j));
+			// intialize the divArea arrayList with the appliances in each location
+			for (int i = 0; i < locationList.size(); ++i) {
+				ArrayList<Appliance> locale = new ArrayList<>();
+				for (int j = 0; j < wholeArea.size(); ++j) {
+					if (locationList.get(i).equals(wholeArea.get(j).getLocation()) && wholeArea.get(j).isOn()) {
+						locale.add(wholeArea.get(j));
 					}
 				}
+				divArea.add(locale);
 			}
-			// checker if all Smart Appliances have been set to low
-			if (T < smartOnApps.size()) {
-				System.out.println(T);
-				++T;
-			} else {
-				count = true;
-			}
-			
-			// once count is true, the simulation will start browning out the locations with the least amount of appliances that are on
-			if (count) {
+			/////////////////////////////////////////
+			//////// Start of the process for turning appliances off
+			///////////////////////////////////////////////////////////
+			System.out.println("Total Wattage: " + totalWattage);
+			while (totalWattage > maxWattage) {
 				
-				int applianceCount = divArea.get(0).size();
-				int locationLow = 0;
-				for (int i = 0; i < divArea.size(); ++i) {
-					if (applianceCount > divArea.get(i).size()) {
-						//System.out.println("Low: "+divArea.get(i).size());
-						applianceCount = divArea.get(i).size();
-						locationLow = i;
-					}
-				}
-				
-				locationList.remove(locationLow);
-				divArea.remove(locationLow);
+				totalWattage = 0;
 				smartOnApps.clear();
 				onApps.clear();
 				
@@ -145,41 +155,87 @@ public class PowerTester {
 						}
 					}
 				}
-				T = smartOnApps.size();
-			} 
-			
-			
-			for (int i = 0; i < smartOnApps.size(); ++i) {				
-				smartOnApps.get(i).setOnLow(true);
-			}
-			
-			// comparator and collections to sort the Smart Appliances from lowest to highest wattage
-			Comparator<SmartAppliance> comparator = Comparator.comparing(SmartAppliance::getOnWattage);
-			
-			Collections.sort(smartOnApps, comparator);
+				
+				for (int i = 0; i < smartOnApps.size(); ++i) {				
+					smartOnApps.get(i).setOnLow(true);
+				}
+				
+				// checker if all Smart Appliances have been set to low
+				if (T < smartOnApps.size()) {
+					++T;
+					// comparator amd collection to sort the Smart Appliances from lowest to highest wattage
+					Comparator<SmartAppliance> comparator = Comparator.comparing(SmartAppliance::getOnWattage);
+					
+					Collections.sort(smartOnApps, comparator);
 
-			for (int i = T; i < smartOnApps.size(); ++i) {
-				smartOnApps.get(i).setOnLow(false);
+					for (int i = T; i < smartOnApps.size(); ++i) {
+						smartOnApps.get(i).setOnLow(false);
+					}
+					
+				} else {
+					
+					++locationCounter;
+					int applianceCount = divArea.get(0).size();
+					int locationLow = 0;			
+					
+					// for loop to find the location with the least amount of On Appliances
+					
+					for (int i = 0; i < divArea.size(); ++i) {
+						if (applianceCount > divArea.get(i).size()) {
+							applianceCount = divArea.get(i).size();
+							locationLow = i;
+						}
+					}
+					totalLocations.remove(locationLow);
+					divArea.remove(locationLow);
+					smartOnApps.clear();
+					onApps.clear();
+					
+					for (int i = 0; i < divArea.size(); ++i) {
+						ArrayList<Appliance> currentLocation = divArea.get(i);
+						for (int j = 0; j < currentLocation.size(); ++j) {
+							if (currentLocation.get(j).getType()) {
+								smartOnApps.add((SmartAppliance) currentLocation.get(j));
+							} else {
+								onApps.add(currentLocation.get(j));
+							}
+						}
+					}
+					T = smartOnApps.size();
+				}
+				
+				for (int i = 0; i < onApps.size(); ++i) {
+					totalWattage += onApps.get(i).getOnWattage();
+				}
+				
+				for (int i = 0; i < smartOnApps.size(); ++i) {
+					totalWattage += smartOnApps.get(i).getOnWattage();
+				}
+				
+				
 			}
 			
-			for (int i = 0; i < onApps.size(); ++i) {
-				totalWattage += onApps.get(i).getOnWattage();
-			}
+			/////////////////////////////////////////
+			//////// Printing data from the Time Step
+			///////////////////////////////////////////////////////////
 			
-			for (int i = 0; i < smartOnApps.size(); ++i) {
-				//System.out.println("Smart" + smartOnApps.get(i).getOnWattage());
-				totalWattage += smartOnApps.get(i).getOnWattage();
-			}
+			System.out.println("Total Wattage After: " + totalWattage);
+			System.out.println("Ammount of Smart Appliances set to Low: " + T);
+			System.out.println("Ammount of Locations Browned Out:" + locationCounter);
+			System.out.println();
+			fileWrite(k);
 			
-			System.out.println("total Watt: " + totalWattage);
 		}
+	}
+	
+	public static void addApplication() {
+		System.out.println("Filler");
 	}
 	
 	public static void main(String[] args) {
 
 		//User interactive part
 		String option1;
-		
 		while(true){// Application menu to be displayed to the user.
 			System.out.println("Select an option:");
 			System.out.println("Type \"A\" Add an appliance");
@@ -191,7 +247,7 @@ public class PowerTester {
 			option1 = scnr.nextLine();
 			
 			if (option1.equals("A")) {
-				
+				addApplication();
 			}
 			else if (option1.equals("D")) {
 				
