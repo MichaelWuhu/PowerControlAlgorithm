ef395c20c8b32ba456e75f17c733637dda9e9bf6
RolanDuclayna <rkduclayna@cpp.edu>
2023-12-09 18:42:02 -0800

    Updated SmartAppliance
    added super.getOnWattage()

--- SmartAppliance.java
+++ SmartAppliance.java
@@ -16,6 +16,7 @@ public class SmartAppliance extends Appliance{
 
 
     public int getOnWattage() { //checks to see whether to return normal watts if onLow is false, or low watts if onLow is true
+        super.getOnWattage();
         if (onLow == true) {
             return (int) ((double)(onWattage) * lowModeReduction); //returns low wattage (normal onWatts times lowMode percentage)
         }
