3ed16677972c54278373e0f6f354d71354180ca4
ElijahRosal <148596994+ElijahRosal@users.noreply.github.com>
2023-12-10 20:11:34 -0800

    added toString

--- SmartAppliance.java
+++ SmartAppliance.java
@@ -3,7 +3,7 @@ public class SmartAppliance extends Appliance{
     private double lowModeReduction;
     boolean onLow;
     private int onWattage = super.getOnWattage();
-
+	
     public SmartAppliance(boolean type, int onWattage, double probability, int location, String applianceId, double lowModeReduction) { // initializes SmartAppliance object
         super(applianceId, onWattage, probability, location, applianceId, isSmart);
         this.onLow = onLow;
@@ -17,6 +17,11 @@ public class SmartAppliance extends Appliance{
 		return this.onLow;
 	}
 
+	@Override
+	public String toString() {
+		return super.toString() + "," + lowModeReduction;
+	}
+	
     public int getOnWattage() { //checks to see whether to return normal watts if onLow is false, or low watts if onLow is true
         if (onLow == true) {
             return (int) ((double)(onWattage) * lowModeReduction); //returns low wattage (normal onWatts times lowMode percentage)
